d0cf6a24a0ba6f6ad25c2b8634167ec5617beb75
Mario Gunter Simao <mario.simao@cern.ch>
2021-05-19 17:53:50 -0300

    Return a result with useful data on node analysis

--- src/Circuit.ts
+++ src/Circuit.ts
@@ -1,7 +1,14 @@
 import {
-  add, lusolve, matrix, zeros,
+  add, complex, Complex, lusolve, Matrix, matrix, zeros,
 } from 'mathjs';
 import Component from './components/Component';
+
+interface NodalAnalysisResult {
+  type: string,
+  name: string,
+  unit: string,
+  value: Complex,
+}
 
 export default class Circuit {
   private components: Component[];
@@ -10,7 +17,7 @@ export default class Circuit {
     this.components = components;
   }
 
-  public nodalAnalysis() {
+  public nodalAnalysis(): NodalAnalysisResult[] {
     const originalEquationSize = this.originalEquationSize();
     const modifiedEquationSize = this.modifiedEquationSize();
 
@@ -23,7 +30,26 @@ export default class Circuit {
 
     let currentExtraIndex = originalEquationSize - 1;
 
+    const results = [] as NodalAnalysisResult[];
+    for (let i = 1; i <= originalEquationSize; i += 1) {
+      results.push({
+        type: 'Node',
+        name: `${i + 1}`,
+        unit: 'V',
+        value: complex(0, 0),
+      });
+    }
+
     this.components.forEach((c) => {
+      for (let ci = 0; ci < c.addedDimensions; ci += 1) {
+        results.push({
+          type: 'Component',
+          name: c.name,
+          unit: 'A',
+          value: complex(0, 0),
+        });
+      }
+
       currentExtraIndex += c.addedDimensions;
 
       const componentConductanceMatrix = c.conductanceMatrix(
@@ -43,7 +69,15 @@ export default class Circuit {
       currentVector = add(currentVector, componentCurrentVector);
     });
 
-    return lusolve(conductanceMatrix, currentVector);
+    const voltageVector = lusolve(conductanceMatrix, currentVector) as Matrix;
+
+    let index = 0;
+    voltageVector.forEach((vectorElement: Complex) => {
+      results[index].value = vectorElement;
+      index += 1;
+    });
+
+    return results;
   }
 
   private originalEquationSize(): number {
