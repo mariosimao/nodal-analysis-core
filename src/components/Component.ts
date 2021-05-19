36fb151aea28dcac585d819b1032b71806841d3c
Mario Gunter Simao <mario.simao@cern.ch>
2021-05-19 15:28:04 -0300

    Define Component interface

---
+++ src/components/Component.ts
@@ -0,0 +1,22 @@
+import { Matrix } from 'mathjs';
+import Node from '../Node';
+import Source from '../sources/Source';
+
+export default interface Component {
+  name: string;
+  nodes: Node[];
+  addedDimensions: number;
+  hasSource: boolean;
+  source: Source | null;
+
+  conductanceMatrix(
+    equationSize: number,
+    currentExtraIndex: number,
+    frequency: number,
+  ): Matrix;
+
+  currentSourceVector(
+    equationSize: number,
+    currentExtraIndex: number,
+  ): Matrix;
+}
